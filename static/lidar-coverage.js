e19fdb0f7a81897a8e97d7608c11f40580a76193
Benj Dag <bjdag1234@gmail.com>
2019-06-23 19:50:23 +0800

    final for static

--- static/lidar-coverage.js
+++ static/lidar-coverage.js
@@ -16,7 +16,7 @@ window.onload=function(){
 
   tile_layer = L.tileLayer.wms('https://lipad.dream.upd.edu.ph/geoserver/wms', {
     ptype: "gxp_wmsgetfeatureinfo",
-    layers: 'geonode:philgrid',
+    layers: 'lipad:philgrid',
     format: 'image/png',
     transparent: true,
     'opacity':1,
